90f6a9516222bb3034b9a2ad3c389beae8786413
Mehmet ali <79807829+mehmet5643@users.noreply.github.com>
2021-12-28 17:04:24 +0300

    master| sifre txt oluşturuldu

--- main.py
+++ main.py
@@ -1,61 +1,80 @@
 import myLib
 
-class Ekle:
+class blockchain:
     def __init__(self):
         self.chain = [self.genesisCreate]
-    def genesisCreate(self):
 
-        return  myLib.sifrelemeYontemleri("sign of Eflatun",)
-    def addBlock(self,data):
-        node=myLib.sifrelemeYontemleri(data,self.chain[-1].hash)
-        #with open("dosyaadı", "r") as dosya: print(dosya.read())
+    def genesisCreate(self):
+        genesis = myLib.sifrelemeYontemleri("sign of Eflatun")
+        return genesis.sha256()
+
+    def addBlock(self, data):
+        previous_hash = str(hash(self.chain[-1]))
+        node = myLib.sifrelemeYontemleri(data, previous_hash)
         self.chain.append(node)
+
     def txtEkle(self):
-        pass
+        with open("sifre.txt", "a+") as dosya:
+            for i in range(len(self.chain)):
+                magic = self.chain[i]
+                dosya.write(f"{str(hash(magic))}\n")
 
 
 while True:
-    result=int(input("1)Turkce yazım kurallari kontrolu\n 2)Block Ekle|"))
+    result = int(input("1)Turkce yazım kurallari kontrolu\n2)Sifreleme|\t"))
 
-    if result==1:
+    if result == 1:
 
-        girdi=input("Cumlenizi giriniz: \t")
+        girdi = input("Cumlenizi giriniz: \t")
         data1 = myLib.dilKontrol(girdi)
-        result1=int(input("1)Kelimelere Ayırma\n 2)Cumlelere Ayirma \n 3)Kucuk unlu uyumu \n 4)Buyuk unlu uyumu\n 5)Sesli Harf sayısı\t"))
-        if result1==1:
+        result1 = int(input(
+            "1)Kelimelere Ayırma\n 2)Cumlelere Ayirma \n 3)Kucuk unlu uyumu \n 4)Buyuk unlu uyumu\n 5)Sesli Harf sayısı\t"))
+        if result1 == 1:
             data1.kelimelereAyirma()
-        elif result1==2:
+        elif result1 == 2:
             data1.cumlelereAyirma()
-        elif  result1==3:
+        elif result1 == 3:
             print(data1.kucukUnluUyumu())
-        elif result1==4:
-            data1.buyukUnluUyumu()
-        elif result1==5:
+        elif result1 == 4:
+            print(data1.buyukUnluUyumu())
+        elif result1 == 5:
             data1.sesliHarfSayisi()
         else:
             print("Eksik veya Hatalı tuşladınız.")
-    if result==2:
-        result1=input("data giriniz : \t")
-        hash1=myLib.sifrelemeYontemleri(result1)
-        ozelSifreleme=int(input("Özel bir sifrelme turu ister misiniz?\n1)YES\n2)no"))
+    if result == 2:
+        result2 = input("data giriniz : ")
+        hash1 = myLib.sifrelemeYontemleri(result2)
+        ozelSifreleme = int(input("Özel bir sifrelme turu ister misiniz?\n1)YES\n2)no"))
 
-        if ozelSifreleme==1:
+        block1 = blockchain()
+        yontem = hash1.sha256()
+        if ozelSifreleme == 1:
             girdiYontem = int(input("1)sha256\n2)md5\n3)sha1\n4)sha512\n5)sha224"))
-            if girdiYontem==1:
-                print(hash1.sha256())
-            elif girdiYontem==2:
-                print(hash1.md5())
-            elif girdiYontem==3:
-                print(hash1.sha1())
-            elif girdiYontem==4:
-                print(hash1.sha512())
-            elif girdiYontem==5:
-                print(hash1.sha224())
-            elif girdiYontem==6:
+            if girdiYontem == 1:
+                yontem = hash1.sha256()
+                print(yontem)
+            elif girdiYontem == 2:
+                yontem = hash1.md5()
+                print(yontem)
+            elif girdiYontem == 3:
+                yontem = hash1.sha1()
+                print(yontem)
+            elif girdiYontem == 4:
+                yontem = hash1.sha512()
+                print(yontem)
+            elif girdiYontem == 5:
+                yontem = hash1.sha224()
+                print(yontem)
+            elif girdiYontem == 6:
                 pass
-            elif girdiYontem==7:
+            elif girdiYontem == 7:
                 pass
 
         else:
             print(hash1.sha256())
-
+        saveBlock = int(input("block u kaydetmek istiyor musun ?\ \t1)YES\n2)no"))
+        if saveBlock == 1:
+            block1.addBlock(result2)
+            block1.txtEkle()
+        else:
+            print("block kaydedilmedi")
--- myLib.py
+++ myLib.py
@@ -1,6 +1,7 @@
 import hashlib
 import time
 
+
 class dilKontrol:
     def __init__(self, kelime):
         self.kelime = kelime
@@ -17,14 +18,15 @@ class dilKontrol:
         kalinUnlu = ["a", "ı", "o", "u"]
         inceUnlu = ["e", "i", "ö", "ü"]
         for kelime in cumleAyirma:
-            if (sum(kelime.count(kalin) for kalin in kalinUnlu)) != 0 and (sum(kelime.count(ince) for ince in inceUnlu)) != 0:
+            if (sum(kelime.count(kalin) for kalin in kalinUnlu)) != 0 and (
+            sum(kelime.count(ince) for ince in inceUnlu)) != 0:
                 return f"{kelime} büyük unlu uyumuna uymaz"
             else:
                 return f"{kelime} büyük unlu uyumuna uyar"
 
     def kucukUnluUyumu(self):
-        unluler = ["a", "ı", "o", "u","i","ö","ü","e"]
-        cumleAyirma=self.kelime.split()
+        unluler = ["a", "ı", "o", "u", "i", "ö", "ü", "e"]
+        cumleAyirma = self.kelime.split()
         myList = []
         for harf in cumleAyirma:
             if harf in unluler:
@@ -57,11 +59,12 @@ class dilKontrol:
     def sesliHarfSayisi(self):
         sesli_harf = 'AEIİOÖUÜaeıioöuü'
         cumleAyirma = self.kelime.split()
-        count=0
+        count = 0
         for kelime in cumleAyirma:
             if kelime in sesli_harf:
-                count+=1
+                count += 1
         print(count)
+
     def cumlelereAyirma(self):
         cumleyeAyirma = self.kelime.split()
         for kelime in range(len(cumleyeAyirma)):
@@ -70,7 +73,6 @@ class dilKontrol:
 
 class sifrelemeYontemleri:
 
-
     def __init__(self, data, previousHash=""):
 
         self.data = data
@@ -78,13 +80,12 @@ class sifrelemeYontemleri:
         self.timeStamp = time.ctime()
         self.kuvvet = 0
 
-
-
     def md5(self):
 
         while True:
             self.kuvvet += 1
-            transaction = hashlib.md5((str(self.timeStamp) + str(self.data) + str(self.previousHash) + str(self.kuvvet)).encode()).hexdigest()
+            transaction = hashlib.md5(
+                (str(self.timeStamp) + str(self.data) + str(self.previousHash) + str(self.kuvvet)).encode()).hexdigest()
             if transaction[0:2] == "00":
                 break
         return transaction
@@ -92,7 +93,8 @@ class sifrelemeYontemleri:
     def sha256(self):
         while True:
             self.kuvvet += 1
-            transaction = hashlib.sha256((str(self.timeStamp) + str(self.data) + str(self.previousHash) + str(self.kuvvet)).encode()).hexdigest()
+            transaction = hashlib.sha256(
+                (str(self.timeStamp) + str(self.data) + str(self.previousHash) + str(self.kuvvet)).encode()).hexdigest()
             if transaction[0:2] == "00":
                 break
         return transaction
@@ -100,7 +102,8 @@ class sifrelemeYontemleri:
     def sha1(self):
         while True:
             self.kuvvet += 1
-            transaction = hashlib.sha1((str(self.timeStamp) + str(self.data) + str(self.previousHash) + str(self.kuvvet)).encode()).hexdigest()
+            transaction = hashlib.sha1(
+                (str(self.timeStamp) + str(self.data) + str(self.previousHash) + str(self.kuvvet)).encode()).hexdigest()
             if transaction[0:2] == "00":
                 break
         return transaction
@@ -108,7 +111,8 @@ class sifrelemeYontemleri:
     def sha224(self):
         while True:
             self.kuvvet += 1
-            result = hashlib.sha224((str(self.timeStamp) + str(self.data) + str(self.previousHash) + str(self.kuvvet)).encode()).hexdigest()
+            result = hashlib.sha224(
+                (str(self.timeStamp) + str(self.data) + str(self.previousHash) + str(self.kuvvet)).encode()).hexdigest()
             if result[0:2] == "00":
                 break
         return result
@@ -116,7 +120,8 @@ class sifrelemeYontemleri:
     def sha512(self):
         while True:
             self.kuvvet += 1
-            result = hashlib.sha512((str(self.timeStamp) + str(self.data) + str(self.previousHash) + str(self.kuvvet)).encode()).hexdigest()
+            result = hashlib.sha512(
+                (str(self.timeStamp) + str(self.data) + str(self.previousHash) + str(self.kuvvet)).encode()).hexdigest()
             if result[0:2] == "00":
                 break
         return result
